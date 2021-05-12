786e5f0ec655719fe82444cc4fea737756324169
wass01 <maatallawassim01@gmail.com>
2021-05-12 17:19:37 +0200

    comics completo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,21 @@
 
 @section('main')
   <div class="comics">
-    <div class="comics-child container">
+    <div class="container">
       <button class="btn-jumbo" type="button" name="button">
         current series
       </button>
-      <!-- @foreach($comics as $key)
-        <p>{{ $key['title'] }}</p>
-      @endforeach -->
-
+    </div>
+    <div class="comics-child container">
+      @foreach($comics as $key)
+        <div class="comics-card">
+          <img src= {{ $key['thumb'] }} alt="">
+          <p>{{ $key['series'] }}</p>
+        </div>
+      @endforeach
+    </div>
+    <div class="load">
+      <button type="button" name="button">load more</button>
     </div>
   </div>
 @endsection
