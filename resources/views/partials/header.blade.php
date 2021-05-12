794d8291d3c740acca485f5b8a1fbec6f14483e0
wass01 <maatallawassim01@gmail.com>
2021-05-12 15:46:45 +0200

    navbar

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,54 @@
+<!-- header -->
+<header>
+  <div class="headtop">
+    <div class="container">
+      <p>dc power</p>
+      <p>additional dc sites</p>
+    </div>
+  </div>
+
+  <nav class="navbar container">
+    <div id="logo">
+      <img src="./img/dc-logo.png" alt="logo">
+    </div>
+    <div class="listnav">
+      <ul>
+        <li>
+          <a href="#">character</a>
+        </li>
+        <li>
+          <a href="#">comics</a>
+        </li>
+        <li>
+          <a href="#">movies</a>
+        </li>
+        <li>
+          <a href="#">tv</a>
+        </li>
+        <li>
+          <a href="#">games</a>
+        </li>
+        <li>
+          <a href="#">collectibles</a>
+        </li>
+        <li>
+          <a href="#">videos</a>
+        </li>
+        <li>
+          <a href="#">fans</a>
+        </li>
+        <li>
+          <a href="#">news</a>
+        </li>
+        <li>
+          <a href="#">shop <i class="fas fa-sort-down"></i></a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="search">
+      <input type="text" placeholder="Search">
+    </div>
+
+  </nav>
+</header>
