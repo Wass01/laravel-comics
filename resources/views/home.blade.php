79b0c9b46248ffa6c599d81a228b554f0ef39f21
wass01 <maatallawassim01@gmail.com>
2021-05-12 17:36:40 +0200

    main finito inizio footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,4 +19,29 @@
       <button type="button" name="button">load more</button>
     </div>
   </div>
+
+  <div class="info">
+    <div class="container info-child">
+      <div class="info-card">
+        <img src="./img/buy-comics-digital-comics.png" alt="">
+        <p>digital comics</p>
+      </div>
+      <div class="info-card">
+        <img src="./img/buy-comics-merchandise.png" alt="">
+        <p>dc merchandise</p>
+      </div>
+      <div class="info-card">
+        <img src="./img/buy-comics-subscriptions.png" alt="">
+        <p>dc subscriptions</p>
+      </div>
+      <div class="info-card">
+        <img src="./img/buy-comics-shop-locator.png" alt="">
+        <p>comics shop locator</p>
+      </div>
+      <div class="info-card">
+        <img src="./img/buy-dc-power-visa.svg" alt="">
+        <p>dc power visa</p>
+      </div>
+    </div>
+  </div>
 @endsection
