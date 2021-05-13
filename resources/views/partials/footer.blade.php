76d6c326af8e25d189546fc6231ba29e6f2ed79f
wass01 <maatallawassim01@gmail.com>
2021-05-13 10:41:27 +0200

    layout homepage finito no responsive

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,65 @@
 <footer>
   <div class="jumbofoot">
+    <div class="container first-list">
+      <ul>
+        <li><h3>DC COMICS</h3></li>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">News</a></li>
+      </ul>
+      <ul>
+        <li><h3>DC</h3></li>
+        <li><a href="#">Term of Use</a></li>
+        <li><a href="#">Privacy Police (New)</a></li>
+        <li><a href="#">Ad Choices</a></li>
+        <li><a href="#">Advertising</a></li>
+        <li><a href="#">Jobs</a></li>
+        <li><a href="#">Subscriptions</a></li>
+        <li><a href="#">Talent Workshops</a></li>
+        <li><a href="#">CPSC Certificates</a></li>
+        <li><a href="#">Ratings</a></li>
+        <li><a href="#">Shop Help</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+      <ul>
+        <li><h3>SITES</h3></li>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">News</a></li>
+      </ul>
+    </div>
+    <div class="container second-list">
+      <ul>
+        <li><h3>SHOP</h3></li>
+        <li><a href="#">Shop DC</a></li>
+        <li><a href="#">Shop DC Collectibles</a></li>
+      </ul>
+      <p class="pfoot">
+        All Site Content TM and © 2020 DC Entertainment, unless otherwise <span>noted here. </span> All rights reserved. <span>Cookies Settings
+        </span>
+      </p>
+    </div>
     <img src="./img/dc-logo-bg.png" alt="">
+  </div>
+  <div class="last-foot">
+    <div class="container">
+      <button type="button" name="button">Sign-up Now</button>
+      <div class="social">
+        <h3>FOLLOW US</h3>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+        <a href="#"><i class="fab fa-pinterest-p"></i></a>
+        <a href="#"><i class="fab fa-periscope"></i></a>
+      </div>
+    </div>
   </div>
 </footer>
